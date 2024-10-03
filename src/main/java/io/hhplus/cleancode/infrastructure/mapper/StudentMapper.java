e29126c9c71cd5f0ca2e03c40347aff0b3d40a03
skr1995 <pc95@naver.com>
2024-10-03 22:05:39 +0900

    old mapper 삭제

--- src/main/java/io/hhplus/cleancode/infrastructure/mapper/StudentMapper.java
+++ src/main/java/io/hhplus/cleancode/infrastructure/mapper/StudentMapper.java
@@ -4,17 +4,19 @@ import io.hhplus.cleancode.domain.entity.Student;
 import io.hhplus.cleancode.infrastructure.entity.StudentEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class StudentMapper {
 
-    @Autowired
-    static ModelMapper modelMapper;
-
-    public static StudentEntity  studentToEntity (Student student) {
-        return modelMapper.map(student,StudentEntity.class);
-    }
-
-    public static Student studentToPojo (StudentEntity studentEntity) {
-        return modelMapper.map(studentEntity,Student.class);
-    }
+//    @Autowired
+//    static ModelMapper modelMapper;
+//
+//    public StudentEntity  studentToEntity (Student student) {
+//        return modelMapper.map(student,StudentEntity.class);
+//    }
+//
+//    public  Student studentToPojo (StudentEntity studentEntity) {
+//        return modelMapper.map(studentEntity,Student.class);
+//    }
 }
--- src/main/java/io/hhplus/cleancode/infrastructure/mapper/SugangMapper.java
+++ src/main/java/io/hhplus/cleancode/infrastructure/mapper/SugangMapper.java
@@ -4,17 +4,19 @@ import io.hhplus.cleancode.domain.entity.Sugang;
 import io.hhplus.cleancode.infrastructure.entity.SugangEntity;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class SugangMapper {
 
-    @Autowired
-    static ModelMapper modelMapper;
-
-    public static SugangEntity SugangToEntity  (Sugang sugang) {
-        return modelMapper.map(sugang,SugangEntity.class);
-    }
-
-    public static Sugang SugangToPojo (SugangEntity sugangEntity) {
-        return modelMapper.map(sugangEntity,Sugang.class);
-    }
+//    @Autowired
+//    static ModelMapper modelMapper;
+//
+//    public  SugangEntity SugangToEntity  (Sugang sugang) {
+//        return modelMapper.map(sugang,SugangEntity.class);
+//    }
+//
+//    public  Sugang SugangToPojo (SugangEntity sugangEntity) {
+//        return modelMapper.map(sugangEntity,Sugang.class);
+//    }
 }
